7e4ef7dbd802a895348aa5cfd05da3be873473b2
lockseed <liuqi2072@gmail.com>
2024-06-05 18:29:00 +0800

    删除部分不用的依赖。

--- src/main.js
+++ src/main.js
@@ -3,10 +3,8 @@ import App from './App.vue'
 import router from './router'
 import { createPinia } from "pinia";
 import vuetify from './plugins/vuetify'
-import hljs from 'highlight.js'
 import CodeEditor from "simple-code-editor";
 import { loadFonts } from './plugins/webfontloader'
-import 'vue-highlight-code/dist/style.css';
 
 const pinia = createPinia()
 loadFonts()
